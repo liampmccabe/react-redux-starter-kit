bf85eddd8fa145091010df161218d6323cf81415
Liam McCabe <liampmccabe@gmail.com>
2016-07-02 15:04:14 +0100

    Revert server to ES5, babel-node currently isn't advised for production

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express'
-const app = express()
+var express = require('express')
+var app = express()
 
 app.use('/', express.static(__dirname + '/build'))
 
-app.listen(3000, () => {
+app.listen(3000, function() {
   console.log('React Redux boilerplate listening on port 3000');
 });
