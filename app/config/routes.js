c612ac43e020ee46e744179f97ebb843e20c1848
Jamie Barton <jamie@launchmade.com>
2016-07-17 12:03:13 +0100

    Added DevTools

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import { App } from '../containers'
-import { About, MoreInfo, Home } from '../components'
+import { Home, About, MoreInfo} from '../components'
 
 export default (
   <Route path="/" component={App}>
